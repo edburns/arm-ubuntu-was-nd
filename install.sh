b915cda5c0484ab3676b5a984d6f1ec4edcf3282
Jianguo Ma <jiangma@microsoft.com>
2019-10-17 09:54:02 +0800

    set debconf frontend as Noninteractive

--- install.sh
+++ install.sh
@@ -25,9 +25,13 @@ repositoryUrl=http://www.ibm.com/software/repositorymanager/com.ibm.websphere.ND
 wasNDTraditional=com.ibm.websphere.ND.v90_9.0.5001.20190828_0616
 ibmJavaSDK=com.ibm.java.jdk.v8_8.0.5040.20190808_0919
 
+# Install package dependencies
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt-get update
+apt-get install unzip -y
+
 # Install IBM Installation Manager
 wget -O "$imKitName" "$imKitLocation"
-sudo apt install unzip
 mkdir im_installer
 unzip "$imKitName" -d im_installer
 ./im_installer/userinstc -log log_file -acceptLicense -installationDirectory IBM/InstallationManager
